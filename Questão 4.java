057900b5200c45b0243e4f2b7081a2515ce0401b
Nathan Ferreira <ntfm95@gmail.com>
2019-10-15 13:09:45 -0300

    Update Questão 4.java

--- Questão 4.java
+++ Questão 4.java
@@ -7,3 +7,39 @@ public void insertProduto(ProdutoEntity produto)
 			.setParameter(2, produto.getPreco())
 			.executeUpdate();
 	}
+-------------------------------------------------------
+	public class ClienteService {
+
+	private EntityManager entityManager;
+	
+	public void insertCliente(ClienteEntity cliente) {
+		entityManager.createQuery("INSERT INTO cliente (nome) VALUES (?)")
+			.setParameter(1, cliente.getNome())
+			.executeUpdate();
+	}
+}
+--------------------------------------------------------
+	public class Produto_Venda {
+
+	private EntityManager entityManager;
+	
+	public void insertProduto(ProdutoVendaEntity produto_venda) {
+		entityManager.createQuery("INSERT INTO produto_venda (quantidade, valor) VALUES (?, ?)")
+		.setParameter(1, produto_venda.getQuantidade())
+		.setParameter(2, produto_venda.getValor())
+		.executeUpdate();
+	}
+}
+---------------------------------------------------------
+	
+	public class VendaService {
+
+	private EntityManager entityManager;
+	
+	public void insertVenda(VendaEntity venda)
+	{
+		entityManager.createNativeQuery("INSERT INTO produto (, data) VALUES (?)")
+			.setParameter(2, venda.getData())
+			.executeUpdate();
+	}
+}
